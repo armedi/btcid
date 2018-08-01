227c96ae5adea7c0c7e8bce9b9463a490da85235
ngeong <pakbosz@gmail.com>
2018-08-01 12:20:46 +0700

    Update common.py

--- vipbtc/common.py
+++ vipbtc/common.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 class APIResponseError(Exception):
     """ Exception raise if the API replies with an HTTP code
@@ -10,14 +11,26 @@ class Session:
         self.__requests_session = requests.Session()
         # self.__requests_session.headers.update({'User-Agent': 'medex'})
 
-    def api_request(self, url, params={}, auth='', http_call='get'):
+    def api_request(self, url, params=None, auth=None, http_call='get'):
         if http_call == 'get':
             response = self.__requests_session.get(url)
         elif http_call == 'post':
             response = self.__requests_session.post(url, data=params, auth=auth)
         
         if not response.ok:
-            msg = "API response error: %s".format(response.status_code)
+            if response.status_code == 429:
+                time.sleep(15)
+            msg = "Status Code: {}".format(response.status_code)
+            raise APIResponseError(msg)
+        else:
+            response = response.json()
+        
+        if "error" in response:
+            msg = "Error: {}".format(response["error"])
+            if "error_description" in response:
+                msg = "{}; Description: {}".format(msg, response["error_description"])
+            if response["error"] == "too_many_requests":
+                time.sleep(15)
             raise APIResponseError(msg)
         
         return response
